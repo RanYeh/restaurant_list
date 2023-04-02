4f4fb9eaa0e10687f5305b816bb83246f822cfc9
RanYeh <kidwiss@gmail.com>
2023-04-02 12:08:51 +0800

    feat: connect to db

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 //-- Include packages & define related varibles
 const express = require('express')
+const mongoose = require('mongoose')
+const exphbrs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
 const port = 3000
 
-const exphbrs = require('express-handlebars')
+//-- Connect to DB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
 
-const restaurantList = require('./restaurant.json')
+db.on('error', () => {
+  console.error('mongodb error')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected')
+})
 
 //-- Set template engine
 app.engine('handlebars', exphbrs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+//-- Use bodyParser
+app.use(express.urlencoded({ extended: true }))
 
 //-- Set static files
 app.use(express.static('public'))
@@ -40,5 +59,5 @@ app.get('/search', (req,res) => {
 
 //-- Start and listen on the express server
 app.listen(port, () => {
-  console.log(`Express is listening on localhost:${port}`)
+  console.log(`Express server is listening on http://localhost:${port}`)
 })
